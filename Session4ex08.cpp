ecc6b0d89cdbd33ba2c76d5143ec2764188007ca
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-24 23:15:02 +0700

    Add files via upload

--- Session4ex08.cpp
+++ Session4ex08.cpp
@@ -8,7 +8,7 @@ int main(){
 	printf("Hay nhap chieu dai canh c: ");
 	scanf("%d",&c);
 	if(a+b>c && b+c>a && c+a>b){
-		printf("Là 3 canh cua tam giac");
+		printf("La 3 canh cua tam giac");
 	}else{
 		printf("Khong phai 3 canh cua tam giac");
 	}
